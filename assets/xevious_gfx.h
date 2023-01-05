8d9e218c3e3712a4ee59e5ddf15cb4f56c36a13b
tcdev42 <msmcdoug@gmail.com>
2023-01-05 22:53:42 +1100

    adding missing header file

---
+++ assets/xevious_gfx.h
@@ -0,0 +1,12 @@
+#ifndef __XEVIOUS_GFX_H__
+#define __XEVIOUS_GFX_H__
+
+#define NUM_COLOURS          128
+#define NUM_BG_TILE_CLUTS    128
+#define NUM_SPRITE_CLUTS     128
+
+#define NUM_FG_TILES         512
+#define NUM_BG_TILES         512
+#define NUM_SPRITES          320
+
+#endif // __XEVIOUS_GFX_H__
