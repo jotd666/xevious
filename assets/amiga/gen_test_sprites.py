d15366d7e165763b5564454658f521be31ce5324
Jean-François Fabre <jotd666@gmail.com>
2023-03-12 17:43:33 +0100

    halway through HW sprite engine refactoring

--- assets/amiga/gen_test_sprites.py
+++ assets/amiga/gen_test_sprites.py
@@ -3,11 +3,13 @@ import bitplanelib,struct
 with open("andor_sprites_dump_1","rb") as f:
     contents = f.read()
 
+new_data = []
 for i in range(0,len(contents),8):
     (tile,state,x,y) = struct.unpack_from(">BBHH",contents,i+2)
     if state == 3:
         print(hex(tile),x,y)
+    new_data.append(contents[i:i+8]+bytes(4))
 
 with open("../../src/amiga/ag_test.68k","w") as f:
     f.write("ag_sprites:")
-    bitplanelib.dump_asm_bytes(contents,f,mit_format=True)
+    bitplanelib.dump_asm_bytes(b"".join(new_data),f,mit_format=True)
