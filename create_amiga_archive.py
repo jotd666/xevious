6b2e249f46f9f8c5e5be06e298d8503bce641a97
Jean-François Fabre <jotd666@gmail.com>
2023-03-31 18:17:05 +0200

    fine tuned build scripts & doc

--- create_amiga_archive.py
+++ create_amiga_archive.py
@@ -1,6 +1,11 @@
-import subprocess
+import subprocess,zipfile
 
 cmd_prefix = ["make","-f","../makefile.am"]
 
 subprocess.check_call(cmd_prefix+["clean"],cwd="src")
+# RELEASE_BUILD=1 turns off all logs & screen flashes
 subprocess.check_call(cmd_prefix+["RELEASE_BUILD=1"],cwd="src")
+# create archive
+with zipfile.ZipFile("Xevious1200.zip","w",compression=zipfile.ZIP_DEFLATED) as zf:
+    for file in ["readme.md","instructions.txt","xevious","xevious.slave","Xevious.info"]:
+        zf.write(file)
