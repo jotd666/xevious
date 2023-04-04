67c52cf61d54edc7e69b1b46d854bfa0c7644061
Jean-François Fabre <jotd666@gmail.com>
2023-04-04 22:19:56 +0200

    60Hz working, fg tile refresh optim

--- create_amiga_archive.py
+++ create_amiga_archive.py
@@ -9,7 +9,7 @@ subprocess.check_call(cmd_prefix+["clean"],cwd="src")
 # RELEASE_BUILD=1 turns off all logs & screen flashes
 subprocess.check_call(cmd_prefix+["RELEASE_BUILD=1"],cwd="src")
 # create archive
-with zipfile.ZipFile("Xevious1200.zip","w",compression=zipfile.ZIP_DEFLATED) as zf:
+with zipfile.ZipFile("Xevious1200_HD.zip","w",compression=zipfile.ZIP_DEFLATED) as zf:
     for file in ["readme.md","instructions.txt","xevious","xevious.slave","Xevious.info"]:
         zf.write(file)
 
